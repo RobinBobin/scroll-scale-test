5becf7a59b0c70065bb617d7607b838664120891
Ruben Shalimov <r_shalimov@inbox.ru>
2022-01-30 22:57:32 +0300

    Gesture.Pan().

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+/**
+ * @format
+ */
+
+import React from "react";
+import {
+  StyleSheet,
+  View
+} from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue
+} from "react-native-reanimated";
+// @ts-ignore
+import kitten from "../assets/kitten.jpg";
+
+const App: React.VFC = () => {
+  const translationX = useSharedValue(0);
+  const translationY = useSharedValue(0);
+  const initialTranslationX = useSharedValue(0);
+  const initialTranslationY = useSharedValue(0);
+  
+  const scale = useSharedValue(1);
+  
+  const imageStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: scale.value
+      },
+      {
+        translateX: translationX.value
+      },
+      {
+        translateY: translationY.value
+      }
+    ]
+  }), []);
+  
+  const gesture = Gesture.Pan()
+    .maxPointers(1)
+    .onEnd(() => {
+      initialTranslationX.value = translationX.value;
+      initialTranslationY.value = translationY.value;
+    })
+    .onUpdate(e => {
+      translationX.value = initialTranslationX.value + e.translationX;
+      translationY.value = initialTranslationY.value + e.translationY;
+    });
+  
+  return (
+    <GestureHandlerRootView
+      style={styles.gestureHandlerRootView}
+    >
+      <GestureDetector
+        gesture={gesture}
+      >
+        <View
+          style={styles.container}
+        >
+          <Animated.Image
+            source={kitten}
+            style={imageStyle}
+          />
+        </View>
+      </GestureDetector>
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "cyan",
+    flex: 1,
+    justifyContent: "center"
+  },
+  gestureHandlerRootView: {
+    flex: 1
+  }
+});
+
+export default App;
