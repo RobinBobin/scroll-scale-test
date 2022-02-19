770dffe66a1cb2602fc8bd331735c14af820d540
Ruben Shalimov <r_shalimov@inbox.ru>
2022-02-19 23:21:36 +0300

    1) src/App.tsx: Gesture.Pinch -> Gesture.Manual. 2) assets/kitten.jpg made twice as small. 3) src/ttt.tsx added.

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,19 @@ import {
 import {
   Gesture,
   GestureDetector,
-  GestureHandlerRootView
+  GestureHandlerRootView,
+  GestureTouchEvent
 } from "react-native-gesture-handler";
 import Animated, {
   useAnimatedStyle,
   useSharedValue
 } from "react-native-reanimated";
-// @ts-ignore
+// @ts-expect-error
 import kitten from "../assets/kitten.jpg";
 
 const App: React.VFC = () => {
-  const savedScale = useSharedValue(1);
+  const pointers = usePointers();
   const scale = useSharedValue(1);
-  
   const start = useSharedValue(THRESHOLD_X);
   const top = useSharedValue(THRESHOLD_Y);
   
@@ -38,44 +38,100 @@ const App: React.VFC = () => {
     ]
   }), []);
   
-  const gesture = Gesture.Race(
+  const getDistance = (...points: Point[]): number => {
+    'worklet'
+    return (
+      Math.sqrt(
+        Math.pow(points[0].x - (points[1]?.x ?? 0), 2)
+        + Math.pow(points[0].y - (points[1]?.y ?? 0), 2)
+      )
+    )
+  }
+  
+  const pan = (event: GestureTouchEvent) => {
+    'worklet'
+    
+    const touch = event.changedTouches[0]
+    const {x, y} = pointers[touch.id].value
+    
+    const xOffset = touch.x - x
+    const yOffset = touch.y - y
+    
+    // console.log(`xOffset: ${xOffset}, yOffset: ${yOffset}`)
+    
+    start.value += xOffset
+    top.value += yOffset
+  }
+  
+  const gesture = Gesture.Exclusive(
+    Gesture.Manual()
+      .onBegin(() => {
+        console.log('onBegin')
+      })
+      .onEnd((_event, success) => {
+        console.log('onEnd', success)
+      })
+      .onFinalize((_event, success) => {
+        console.log('onFinalize', success)
+      })
+      .onStart(() => {
+        console.log('onStart')
+      })
+      .onTouchesDown((event, manager) => {
+        if (
+          (event.numberOfTouches === 1 && event.changedTouches[0].id)
+          || event.changedTouches.find(touch => touch.id >= pointers.length)
+        ) {
+          manager.fail()
+          return
+        }
+        
+        for (const touch of event.changedTouches) {
+          pointers[touch.id].value = {
+            x: touch.x,
+            y: touch.y
+          }
+        }
+      })
+      .onTouchesMove((event, manager) => {
+        manager.activate()
+        
+        if (event.numberOfTouches === 1) {
+          pan(event)
+        } else {
+          // const pointerOffsets = event.changedTouches.map(touch => {
+          //   return getDistance(touch) - getDistance(pointers[touch.id].value)
+          // })
+          
+          // if ((pointerOffsets[0] - pointerOffsets[1]) < 1) {
+          //   pan(event)
+          // } else {
+            const oldDistance = getDistance(pointers[0].value, pointers[1].value)
+            const newDistance = getDistance(...event.allTouches)
+            
+            scale.value *= newDistance / oldDistance
+          // }
+        }
+        
+        for (const touch of event.changedTouches) {
+          pointers[touch.id].value = {
+            x: touch.x,
+            y: touch.y
+          }
+        }
+      })
+      .onTouchesUp((event, manager) => {
+        if (!event.numberOfTouches) {
+          manager.end()
+        }
+      }),
     Gesture.Tap()
       .onStart(() => {
-        savedScale.value = 1;
-        scale.value = 1;
-        
-        start.value = THRESHOLD_X;
-        top.value = THRESHOLD_Y;
-      }),
-    Gesture.Simultaneous(
-      Gesture.Pan()
-        .maxPointers(1)
-        .onChange(e => {
-          "worklet"
-          const newStart = start.value + e.changeX;
-          const newTop = top.value + e.changeY;
-          
-          // = The coordinates of the top-left corner are dependent on the scale = //
-          const thresholdX = THRESHOLD_X + (kittenAssetSource.width * (scale.value - 1) / 2);
-          const thresholdY = THRESHOLD_Y + (kittenAssetSource.height * (scale.value - 1) / 2);
-          
-          if (newStart >= thresholdX) {
-            start.value = newStart;
-          }
-          
-          if (newTop >= thresholdY) {
-            top.value = newTop;
-          }
-        }),
-      Gesture.Pinch()
-        .onEnd(() => {
-          savedScale.value = scale.value;
-        })
-        .onUpdate(e => {
-          scale.value = savedScale.value * e.scale;
-        })
-    )
-  );
+        scale.value = 1
+        start.value = THRESHOLD_X
+        top.value = THRESHOLD_Y
+      })
+  )
   
   return (
     <GestureHandlerRootView
@@ -97,6 +153,18 @@ const App: React.VFC = () => {
   );
 };
 
+interface Point {
+  readonly x: number
+  readonly y: number
+}
+
+interface Pointer extends Point {}
+
+const THRESHOLD_X = 5
+const THRESHOLD_Y = 10
+
+const kittenAssetSource = Image.resolveAssetSource(kitten)
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "cyan",
@@ -105,11 +173,19 @@ const styles = StyleSheet.create({
   gestureHandlerRootView: {
     flex: 1
   }
-});
+})
 
-export default App;
+const usePointers = (): ReadonlyArray <Animated.SharedValue <Pointer>> => {
+  const pointers: Animated.SharedValue <Pointer> [] = []
+  
+  for (let i = 0; i < 2; ++i) {
+    pointers[i] = useSharedValue({
+      x: 0,
+      y: 0
+    })
+  }
+  
+  return pointers
+}
 
-const kittenAssetSource = Image.resolveAssetSource(kitten);
-
-const THRESHOLD_X = 5;
-const THRESHOLD_Y = 10;
+export default App
